0db14d95e2d515201af6e83e0ab9bf595ba8e077
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-04-16 21:19:44 +0200

    refactor: migrate to eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import eslint from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ['dist', 'types/generated'],
+  },
+)
