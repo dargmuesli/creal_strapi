6752d24baf9c292f7d7da0d64eee3364aab7dafd
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-04-18 00:33:23 +0200

    chore(lint): rename variable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import eslint from '@eslint/js'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
+  prettierRecommended,
   {
     languageOptions: {
       globals: {
